36c4519d7f3468245a43a83c30e54ab85cd50caa
Robinson Zapata <ideas.beta1@gmail.com>
2026-04-13 00:09:50 -0500

    chore: exclude scratch_waba.ts from build and update import path to resolve extension mismatch

--- scratch_waba.ts
+++ scratch_waba.ts
@@ -1,6 +1,6 @@
 import { config } from 'dotenv'
 config({ path: '.env.local' })
-import { getTemplates } from './src/utils/waba.ts'
+import { getTemplates } from './src/utils/waba'
 
 async function fetch() {
    const templates = await getTemplates()
